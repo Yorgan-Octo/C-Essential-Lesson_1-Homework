c60dcc238784df42f48d22473e9d03058aeafb1a
Artem Priadchenko <yorgan.info@gmail.com>
2023-04-22 00:45:32 +0300

    Implementation of all the main functionality in the Program class

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -10,6 +10,24 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            UserInterfece userInterfece = new UserInterfece();
+
+            userInterfece.HeadShow("Програма що обчислює площу прямокутника та периметр прямокутника!");
+
+            double namber1 = userInterfece.InDoubleNam("Введите первое значение: ");
+            double namber2 = userInterfece.InDoubleNam("Введите второе значение: ");
+
+            Rectangle rectangle = new Rectangle(namber1, namber2);
+
+            double areaSum = rectangle.AreaCalculator();
+            double perimeterSum = rectangle.PerimeterCalculator();
+
+            //тут не обовясково потрыбно було звертатися до полів rectangle.Side1 мржна булоп росто вказати змінні namber1 та namber2
+            //це так для прикладу
+            userInterfece.HeadShow($"Площа Прямокутника зи стороною {rectangle.Side1} и стороною {rectangle.Side2} буде {areaSum}");
+            userInterfece.HeadShow($"Периметр Прямокутника зи стороною {rectangle.Side1} и стороною {rectangle.Side2} буде {perimeterSum}");
+
+            Console.ReadKey();
         }
     }
 }
