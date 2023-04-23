d201f14f25972288e2babd685b74750a2eea3c29
Artem Priadchenko <yorgan.info@gmail.com>
2023-04-23 14:14:37 +0300

    другий варіан реалізації завдання 3

---
+++ Task_3_v2/Content.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_v2
+{
+    internal class Content
+    {
+
+        private string contentBook;
+
+        public string ContentBook
+        {
+            get
+            {
+                if (contentBook != null)
+                    return contentBook;
+                else
+                    return "Синопсис відсутній.";
+            }
+            set
+            {
+                contentBook = value;
+
+            }
+
+        }
+
+        public void Show()
+        {
+            ConsoleColor color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Синопсис {ContentBook}");
+            Console.ForegroundColor = color;
+        }
+
+
+    }
+}
---
+++ Task_3_v2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Введіть назву книги: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введіть Автора: ");
+            string autor = Console.ReadLine();
+
+            Console.Write("Введіть Синопсис: ");
+            string dis = Console.ReadLine();
+
+
+
+            Book book = new Book(name);
+
+            book.Author = autor;
+            book.Content = dis;
+
+            Console.Clear();
+            book.AllInfoBook();
+
+            Console.ReadKey();
+        }
+    }
+}
